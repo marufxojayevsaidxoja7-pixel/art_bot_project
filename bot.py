d46009c05952c1021fdc04ad4d06b4ce79a8a9ff
marufxojayevsaidxoja7-pixel <marufxojayevsaidxoja7@gmail.com>
2026-06-03 12:40:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 load_dotenv()
 
-API_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+API_TOKEN = os.getenv("8883984125:AAExjmSNL5qxUiK7rUtgOS0uVVOsbT0ran4")
+ADMIN_ID = int(os.getenv("8584102298", "0"))
 
 if not API_TOKEN:
     raise ValueError("BOT_TOKEN muhit o'zgaruvchisi o'rnatilmagan!")
